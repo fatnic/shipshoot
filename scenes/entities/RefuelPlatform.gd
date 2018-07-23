072b2223fb90e58c84d3785cf3351b46c0b94670
Dave Nicholson <fatnic388@gmail.com>
2018-07-23 21:51:19 +0100

    refuel amount added

--- scenes/entities/RefuelPlatform.gd
+++ scenes/entities/RefuelPlatform.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+export (int) var refuel_rate = 10
+
 func _on_Area2D_body_entered(body):
-	if body.has_method("set_refuel_rate"): body.call("set_refuel_rate", 10)
+	if body.has_method("set_refuel_rate"): body.call("set_refuel_rate", refuel_rate)
 
 func _on_Area2D_body_exited(body):
 	if body.has_method("set_refuel_rate"): body.call("set_refuel_rate", null)
